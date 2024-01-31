b1d9334897809e6b4d4df41c56f67c5c58e69161
arisao <ars.1996.jump@gmail.com>
2024-01-31 17:00:45 +0000

    四則演算①

---
+++ calculate.php
@@ -0,0 +1,21 @@
+<?php
+
+echo "calculate \n";
+
+$a = 1 + 2;
+echo $a;
+echo "\n";
+
+echo $a , "\n";
+
+echo "{$a} \n";
+
+//数値を代入
+
+$i = 11;
+$j = 22;
+
+//変数を使った計算
+$k = $i + $j;
+
+echo "{$k} \n";
