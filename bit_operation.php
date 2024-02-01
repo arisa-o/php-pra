d97cbe59dad641e7d1c4b4a01166e15c92f1f7b1
arisao <ars.1996.jump@gmail.com>
2024-02-01 12:55:46 +0000

    ビット単位の演算

---
+++ bit_operation.php
@@ -0,0 +1,26 @@
+<?php
+
+$i = random_int(0, 255);
+$j = random_int(0, 255);
+
+// AND
+$k = $i & $j;
+
+echo "{$i}, {$j} ,{$k} \n" ;
+
+printf("%x & %x = %x \n", $i, $j, $k);
+
+// OR
+$k = $i | $j;
+echo "{$i}, {$j} ,{$k} \n" ;
+printf("%x | %x = %x \n", $i, $j, $k);
+
+// XOR
+$k = $i ^ $j;
+echo "{$i}, {$j} ,{$k} \n" ;
+printf("%x ^ %x = %x \n", $i, $j, $k);
+
+// NOT
+$k = ~$i;
+echo "{$i}, {$j} ,{$k} \n" ;
+printf("~%x = %x \n", $i, $k);
