a39a76f802a2279bd106170073dacca0ac325f31
arisao <ars.1996.jump@gmail.com>
2024-02-03 17:23:26 +0000

    switch文

---
+++ switch.php
@@ -0,0 +1,31 @@
+<?php
+
+$i = 4;
+switch($i){
+	case 0:
+		echo "iは0です \n";
+		break;
+		
+	case 1:
+		echo "iは1です \n";
+		break;
+		
+	default:
+		echo "iは0と1以外です \n";
+		break;
+}
+
+$i = 2;
+switch($i){
+	case '2a':
+		echo "iは　'2a'です \n";
+		break;
+	
+	case '2':
+		echo "iは　'2'(string)です　\n";
+		break;
+		
+	case 2:
+		echo "iは　2(int)です \n";
+		break;
+}
